12047952adf926795f6f604b86f2f7372c15b5ed
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-06-04 17:28:20 +0100

    add helm repo script

---
+++ bin/.local/bin/helm-repo.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+helm repo add paralus https://paralus.github.io/helm-charts
+helm repo add nats https://nats-io.github.io/k8s/helm/charts
+helm repo add external-secrets https://charts.external-secrets.io
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add kyverno https://kyverno.github.io/kyverno/
+helm repo add jetstack https://charts.jetstack.io
+helm repo add stakater https://stakater.github.io/stakater-charts
+helm repo add devtron https://helm.devtron.ai
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+helm repo update
+helm repo list
