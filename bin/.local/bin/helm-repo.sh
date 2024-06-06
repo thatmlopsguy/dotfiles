ddf7446038c3137ce5c77ada38932ab1441f3ffc
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-06-06 16:22:04 +0100

    add otel helm repo

--- bin/.local/bin/helm-repo.sh
+++ bin/.local/bin/helm-repo.sh
@@ -10,6 +10,7 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo add devtron https://helm.devtron.ai
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 
 helm repo update
 helm repo list
