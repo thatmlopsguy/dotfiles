4d1b02b112227f173a66c86d719a06922f400927
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-06-11 17:10:27 +0100

    add installers

---
+++ scripts/install-zsh-tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '[*] Install plugins oh-my-zsh'
+git clone https://github.com/zsh-users/zsh-completions.git "${ZSH_CUSTOM}/plugins/zsh-completions"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+git clone https://github.com/marlonrichert/zsh-autocomplete.git "${ZSH_CUSTOM}/plugins/zsh-autocomplete"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/fast-syntax-highlighting"
+git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git "${ZSH_CUSTOM}/plugins/autoswitch_virtualenv"
+
+echo '[*] Install themes oh-my-zsh'
+git clone https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}/themes/powerlevel10k"
