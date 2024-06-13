2d84f96e8a743b5fcdff042f7201227b0e61992c
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-06-13 15:34:44 +0100

    add k8s at home helm repo

--- bin/.local/bin/helm-repo.sh
+++ bin/.local/bin/helm-repo.sh
@@ -11,6 +11,7 @@ helm repo add stakater https://stakater.github.io/stakater-charts
 helm repo add devtron https://helm.devtron.ai
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+helm repo add k8s-at-home https://k8s-at-home.com/charts
 
 helm repo update
 helm repo list
