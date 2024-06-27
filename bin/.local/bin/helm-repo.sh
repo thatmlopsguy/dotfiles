11222711cb08031bed3f372150356f73c936cf77
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-06-27 09:50:06 +0100

    helm: add radius repo

--- bin/.local/bin/helm-repo.sh
+++ bin/.local/bin/helm-repo.sh
@@ -54,6 +54,7 @@ helm repo add openebs https://openebs.github.io/openebs
 helm repo add signoz https://charts.signoz.io
 helm repo add dapr https://dapr.github.io/helm-charts/
 helm repo add zipkin https://zipkin.io/zipkin-helm
+helm repo add radius https://radius.azurecr.io/helm/v1/repo
 
 helm repo update
 helm repo list
