b7c7d34572805002bfabe5e3e705a24c39fb885b
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-07-07 16:31:28 +0100

    add nix-shell installer

--- scripts/install-devbox.sh
+++ scripts/install-devbox.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# https://nixos.org/download/#nix-install-windows
+sh <(curl -L https://nixos.org/nix/install) --daemon
+nix-shell -p nix-info --run "nix-info -m"
+
 # https://www.jetify.com/devbox/docs/installing_devbox/
 curl -fsSL https://get.jetify.com/devbox | bash
