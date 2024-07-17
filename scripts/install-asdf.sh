cb9589d0f264f4cc5b06721f5ce527764c4f53dd
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-07-17 16:20:02 +0100

    scripts: update asdf version

--- scripts/install-asdf.sh
+++ scripts/install-asdf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo '[*] Install asdf'
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
