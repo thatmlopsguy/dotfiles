a7ac4ef2ebe2749976b12bc2ebbe8d15b42ace18
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-10-08 09:40:52 +0100

    mozilla: disable the built-in translator

--- mozilla/.mozilla/firefox/default/user.js
+++ mozilla/.mozilla/firefox/default/user.js
@@ -1,2 +1,7 @@
+// https://make-firefox-private-again.com/
+
 // userchrome.css usercontent.css activate
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// disable the built-in translator
+user_pref("browser.translations.automaticallyPopup", false);
