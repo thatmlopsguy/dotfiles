f229b4be9d74c94a036f86546a6ef6154cd104bd
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-10-15 10:19:29 +0100

    brew: add datadog mkat

--- bin/.local/bin/brew-update.sh
+++ bin/.local/bin/brew-update.sh
@@ -7,8 +7,10 @@ brew missing
 brew doctor
 
 brew tap aws/tap
+brew tap datadog/mkat https://github.com/datadog/managed-kubernetes-auditing-toolkit
 
 brew install arttor/tap/helmify
 brew install openslo/openslo/oslo
 brew install fortio
 brew install azure-cli
+brew install datadog/mkat/managed-kubernetes-auditing-toolkit
