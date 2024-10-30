c4498308f882d33b04e06a5e695dd0318f3b472d
thatmlopsguy <165834479+thatmlopsguy@users.noreply.github.com>
2024-10-30 09:01:38 +0000

    helm: add kanister repo

--- bin/.local/bin/helm-repo.sh
+++ bin/.local/bin/helm-repo.sh
@@ -95,6 +95,7 @@ helm repo add kubereboot https://kubereboot.github.io/charts
 helm repo add vm https://victoriametrics.github.io/helm-charts
 helm repo add cluster-proportional-autoscaler https://kubernetes-sigs.github.io/cluster-proportional-autoscaler
 helm repo add bakito https://charts.bakito.net
+helm repo add kanister https://charts.kanister.io
 # ML
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia
 helm repo add kuberay https://ray-project.github.io/kuberay-helm
